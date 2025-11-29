b66efad98fa3be7d6e7a7d96ebee18465bf1f45e
Luis Lopez <jorg3luislopezc1941@hotmail.com>
2025-11-29 05:43:23 -0600

    Simplify zonas controller: remove redundant validation for route parameter

--- src/apis/zonas/zonas.controller.ts
+++ src/apis/zonas/zonas.controller.ts
@@ -13,29 +13,8 @@ export class ZonasController {
   }
 
   // Obtener todas las zonas de un usuario (las que recibió)
-  @Get()
-  findAll(@Query('runnerUID') runnerUID: string) {
-    if (!runnerUID) {
-      return {
-        message: 'El parámetro runnerUID es requerido',
-        status: 'error',
-      };
-    }
-    return this.zonasService.findByRunnerUID(runnerUID);
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.zonasService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateZonaDto: UpdateZonaDto) {
-    return this.zonasService.update(+id, updateZonaDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.zonasService.remove(+id);
+  @Get('get-by-runneruid/:runnerUID')
+  findAll(@Param('runnerUID') runnerUID: string, @Query('currentUser') currentUser?: string) {
+    return this.zonasService.findByRunnerUID(runnerUID, currentUser);
   }
 }
