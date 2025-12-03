70dd93b79574d0ba6f04d19e310ed73d01ff6639
Luis Lopez <jorg3luislopezc1941@hotmail.com>
2025-12-03 16:27:49 -0600

    feat: agregar endpoints de filtros de rutas
    - Agregar GET get-by-pais/:pais
    - Agregar GET get-by-ciudad/:ciudad
    - Agregar GET get-by-estado/:estado

--- src/apis/rutas/rutas.controller.ts
+++ src/apis/rutas/rutas.controller.ts
@@ -17,6 +17,21 @@ export class RutasController {
     return this.rutasService.findAll();
   }
 
+  @Get('get-by-pais/:pais')
+  findByPais(@Param('pais') pais: string) {
+    return this.rutasService.findByPais(pais);
+  }
+
+  @Get('get-by-ciudad/:ciudad')
+  findByCiudad(@Param('ciudad') ciudad: string) {
+    return this.rutasService.findByCiudad(ciudad);
+  }
+
+  @Get('get-by-estado/:estado')
+  findByEstado(@Param('estado') estado: string) {
+    return this.rutasService.findByEstado(estado);
+  }
+
   @Get('get-by-id/:id')
   findOne(@Param('id') id: string) {
     return this.rutasService.findOne(+id);
